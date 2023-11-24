831a2af43fdeed6c1b2a589a859dcd234165e5a6
César Rodriguez <54708668+ironsand-sou-eu@users.noreply.github.com>
2023-11-24 20:37:46 +0100

    Updated configs

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-node",
+  testPathIgnorePatterns: ["<rootDir>/nome_modules/", "<rootDir>/dist/"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  transform: {},
+};
+
+export default config;
